d8d5c173bf05733b6f18dcb08f43ad61d089e2f0
Furkan Akal <furkanakaldev@gmail.com>
2023-09-07 14:48:03 +0300

    add: initial query

---
+++ main.js
@@ -0,0 +1,48 @@
+import { ApolloClient, InMemoryCache, gql } from '@apollo/client';
+
+const APIURL = 'https://api.thegraph.com/subgraphs/name/furkanakal/evilusdt/';
+
+const tokensQuery = `
+  query {
+    issues(first: 5, orderBy: blockNumber, orderDirection: desc) {
+      amount
+      blockNumber
+      transactionHash
+    }
+    redeems(first: 5, orderBy: blockNumber, orderDirection: desc) {
+      amount
+      blockNumber
+      transactionHash
+    }
+    addedBlackLists(first: 5, orderBy: blockNumber, orderDirection: desc) {
+      _user
+      blockNumber
+      transactionHash
+    }
+    removedBlackLists(first: 5, orderBy: blockNumber, orderDirection: desc) {
+      _user
+      blockNumber
+      transactionHash
+    }
+    destroyedBlackFunds(first: 5, orderBy: blockNumber, orderDirection: desc) {
+      _balance
+      _blackListedUser
+      blockNumber
+      transactionHash
+    }
+  }
+`;
+
+const client = new ApolloClient({
+  uri: APIURL,
+  cache: new InMemoryCache(),
+});
+
+client
+  .query({
+    query: gql(tokensQuery),
+  })
+  .then((data) => console.log('Subgraph data: ', data))
+  .catch((err) => {
+    console.log('Error fetching data: ', err)
+  });
